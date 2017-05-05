25e3f56017f07cc3a3ae57632aeaa0d329ed3b8e
Sergey <sergii.zagryvyi@dimetogo.com>
2017-05-05 19:03:29 +0300

    add test

--- src/main/java/ua/com/serzh/EmployeeRestAppApplication.java
+++ src/main/java/ua/com/serzh/EmployeeRestAppApplication.java
@@ -3,10 +3,31 @@ package ua.com.serzh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+import java.util.Properties;
+
 @SpringBootApplication
 public class EmployeeRestAppApplication {
+    // Get PORT and HOST from Environment or set default
+    private static final Optional<String> host;
+    private static final Optional<String> port;
+    private static final Properties myProps = new Properties();
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeRestAppApplication.class, args);
-	}
+    static {
+        host = Optional.ofNullable(System.getenv("HOSTNAME"));
+        port = Optional.ofNullable(System.getenv("PORT"));
+    }
+
+    public static void main(String[] args) {
+//		SpringApplication.run(EmployeeRestAppApplication.class, args);
+
+        // Set properties
+        myProps.setProperty("server.address", host.orElse("localhost"));
+        myProps.setProperty("server.port", port.orElse("8083"));
+
+        SpringApplication app = new SpringApplication(EmployeeRestAppApplication.class);
+        app.setDefaultProperties(myProps);
+        app.run(args);
+
+    }
 }
--- src/test/java/ua/com/serzh/EmployeeRestAppApplicationTests.java
+++ src/test/java/ua/com/serzh/EmployeeRestAppApplicationTests.java
@@ -1,5 +1,7 @@
 package ua.com.serzh;
 
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,10 +9,34 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EmployeeRestAppApplicationTests {
+public class EmployeeRestAppApplicationTests extends TestCase {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public EmployeeRestAppApplicationTests(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+
+    public static junit.framework.Test suite() {
+        return new TestSuite(EmployeeRestAppApplicationTests.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
 
 }
