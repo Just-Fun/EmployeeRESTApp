a2798ea4fadb007dc0c7ac085e608df650f08fbe
Sergey <sergii.zagryvyi@dimetogo.com>
2017-05-05 18:45:23 +0300

    start

---
+++ src/main/java/ua/com/serzh/EmployeeRestAppApplication.java
@@ -0,0 +1,12 @@
+package ua.com.serzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestAppApplication.class, args);
+	}
+}
